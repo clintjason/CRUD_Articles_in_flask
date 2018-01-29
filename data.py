a962badae226a6e2e0f6de641dabc13055e92afc
clintjason <ngassajason@yahoo.fr>
2018-01-29 23:15:21 +0100

    data file

---
+++ data.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+def Articles():
+    article = [
+        {'id' : 1,
+        'title' : 'article One',
+         'body' : 'The body of the article',
+         'author' : 'Clint Jason',
+         'create_date' : '07-08-2017'},
+
+        {'id' : 2,
+         'title' : 'article Two',
+         'body' : 'The body of the article 2',
+         'author' : 'Melvine Europe',
+         'create_date' : '07-08-2017'},
+
+        {'id' : 3,
+         'title' : 'article Three',
+         'body' : 'The body of the article 3',
+         'author' : 'Raul Essos',
+         'create_date' : '07-08-2017'}
+    ]
+
+    return article
